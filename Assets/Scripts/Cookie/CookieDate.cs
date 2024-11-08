50ebc628a6dd3320863a8e6e85183d7adc68ab00
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2024-11-08 09:23:12 +0900

    [Add]クッキーをクリックする機能を追加

---
+++ Assets/Scripts/Cookie/CookieDate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// クッキーのデータを管理しています
+/// </summary>
+public class CookieDate
+{
+    private float _cookies = 0; //クッキーの枚数
+    private float _increaseCookie = 1; //1クリックで入手できるクッキーの枚数
+
+    public float Cookies
+    {
+        get { return _cookies; }
+        set { _cookies = value; }
+    }
+    public float IncreaseCookie
+    {
+        get { return _increaseCookie; }
+    }
+
+    /// <summary>
+    /// 1クリックで入手できるクッキーの枚数を増やす
+    /// </summary>
+    /// <param name="mag">増加倍率</param>
+    public void Increase(float mag)
+    {
+        _increaseCookie += _increaseCookie * mag;
+        Debug.Log(_increaseCookie);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    CookieDate _cookieDate;
+
+    private void Start()
+    {
+        _cookieDate = new CookieDate();
+    }
+
+    private void Update()
+    {
+        // ToDO : 施設のCookie増加の処理をここに書き込む
+    }
+
+    public void CookieClicked()
+    {
+        _cookieDate.Cookies += _cookieDate.IncreaseCookie;
+        Debug.Log("Click");
+    }
+}
