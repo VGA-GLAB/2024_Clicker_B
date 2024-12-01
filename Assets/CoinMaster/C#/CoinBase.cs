ded28acea1b61900d98f862afa30ea63214984cb
Oikawa <yucchi0424@gmail.com>
2024-12-01 17:16:34 +0900

    [update]CoinBase.csの> <などの演算子の対応とnew()時にlongやdoubleの対応を実装。

--- Assets/CoinMaster/C#/CoinBase.cs
+++ Assets/CoinMaster/C#/CoinBase.cs
@@ -14,6 +14,16 @@ public class Coin
         this.resource = resource;
     }
 
+    public Coin(long resource)
+    {
+        this.resource = new BigInteger(resource) * (long)digit;
+    }
+
+    public Coin(double resource)
+    {
+        this.resource = new BigInteger(resource * (long)digit);
+    }
+
     public long ToLong()
         => (resource / (long)digit) >= long.MaxValue ? long.MaxValue : (long)(resource / (long)digit);
 
@@ -43,6 +53,11 @@ public class Coin
 
     public static bool operator <=(Coin coin, double amount)
         => (coin - amount).resource <= 0;
+    
+    public static bool operator >(Coin coin, double amount)
+        => (coin - amount).resource > 0;
+    public static bool operator <(Coin coin, double amount)
+        => (coin - amount).resource < 0;
 
     public override string ToString()
         => $"{resource / (int)digit}";
